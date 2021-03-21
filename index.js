0e4faa8f941ff0fbb75f775169b12b4b26577685
Alysty <pppjw@protonmail.ch>
2021-03-21 07:17:14 -0300

    removed image and code that was used for testing in during development

--- index.js
+++ index.js
@@ -5,27 +5,14 @@ const app = express();
 app.use(express.json());
 
 const db = require('./config/database');
-/* 
-Test 
-db.authenticate()
-    .then(() => console.log('Database connected'))
-    .catch(err => console.log(err))
-*/
 
-
-
+// main page of the API
 app.get('/', (req, res)=>{
     res.send('This Api was made by Pedro Paulo Pastore Jorge \n this api is made for an github type app')
 })
 
-/*
-app.get('/api/users/:id', (req,res)=>{
-    res.send(req.params);
-    if ? use req.query to get ?... as object
-})
 
-*/
-
+//Routes of the API
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
